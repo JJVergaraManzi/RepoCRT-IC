132e1a9262dbb215387e77adde06222865b162e0
juanjo2331 <40677444+JJVergaraManzi@users.noreply.github.com>
2021-12-14 02:12:39 -0300

    Minor Update en home

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -12,14 +12,7 @@ class Home extends Component {
           className="input"
           placeholder="Barra de filtro"/>
             
-            <p>Cras facilisis urna ornare ex volutpat, et
-            convallis erat elementum. Ut aliquam, ipsum vitae
-            gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-            metus nec massa. Maecenas hendrerit laoreet augue
-            nec molestie. Cum sociis natoque penatibus et magnis
-            dis parturient montes, nascetur ridiculus mus.</p>
-    
-            <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            <p>Prueba de Home</p>
         </div>
     );
   }
